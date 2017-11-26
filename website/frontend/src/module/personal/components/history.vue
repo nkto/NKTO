2bb32650084cd2f1f32433a69fbcb92104fe10b3
a1liz <694426737@qq.com>
2017-11-26 15:30:25 +0800

    更新了浏览历史以及已下订单的界面

--- website/frontend/src/module/personal/components/history.vue
+++ website/frontend/src/module/personal/components/history.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
-      history
+      <Card>
+        <Row>
+          <Col span="6"><img src="/static/img/a1.jpg" height="180px"></Col>
+          <Col span="15" >
+            <div>
+              <h2>
+                [九成新] 操作系统笔记
+              </h2>
+            </div>
+            <div style ="float: right;margin-top:20px;font-size:15px">
+              价格：25元
+            </div>
+            <div style ="margin-top: 50px">
+              <h4>
+              上架时间：2017/11/01
+              </h4>
+            </div>
+            <div style ="float: none;">
+              分类：学习资料
+            </div>
+            
+          </Col>
+        </Row>
+      </Card>
   </div>
 </template>
 <script>
